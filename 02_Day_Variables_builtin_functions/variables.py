32c44ba4758c9c22872e6f27e8dcf0f2df4e817f
Nagraj <naiknagrj@gmail.com>
2026-01-04 15:12:03 +0530

    test

--- 02_Day_Variables_builtin_functions/variables.py
+++ 02_Day_Variables_builtin_functions/variables.py
@@ -12,7 +12,7 @@ person_info = {
     'firstname':'Nagraj',
     'lastname':'Naik',
     'country':'India',
-    'city':'Karwar'
+    'city':'Gotham'
     }
 
 # Printing the values stored in the variables
@@ -30,7 +30,7 @@ print('Person information: ', person_info)
 
 # Declaring multiple variables in one line
 
-first_name, last_name, country, age, is_married = 'Nagraj', 'Naik', 'India', 250, True
+first_name, last_name, country, age, is_married = 'Nagraj', 'Naik', 'India', 69, True
 
 print(first_name, last_name, country, age, is_married)
 print('First name:', first_name)
